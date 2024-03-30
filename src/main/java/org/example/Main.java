e804a56d116c86986ee6137f211e7c0cf73942ab
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-03-30 22:30:01 +0300

    Готово

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.example.core.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person1 = new Person("Иванов", "Иван", 20);
+        System.out.println(person1);
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        String json = gson.toJson(person1);
+        System.out.println(json);
+        Person person2 = new Gson().fromJson(json, Person.class);
+        System.out.println(person2);
     }
 }
